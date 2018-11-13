3065e0fd90dace327df26dc86aab1cfc65063811
kamild160 <293206@uwr.edu.pl>
2018-11-13 11:57:48 +0100

    Dodoano testy

---
+++ klasy.cpp
@@ -0,0 +1,27 @@
+//#include "klasy.h"
+//#include <iostream>
+//#include <vector>
+//#include <cmath>
+//#include "klasy.h"
+//#include <gtest/gtest.h>
+//#include <gmock/gmock.h>
+//using namespace std;
+//
+//int main(){
+//
+//    myexception myex;
+//
+//    Kwadrat kw(2,0,1);
+//
+//    unsigned int a=0;
+//
+//
+//
+//    try{
+//        cout<<kw.miejscezerowe(a, myex);
+//    }catch (exception& e){
+//        cout<<e.what();
+//    }
+//
+//    return 0;
+//}
---
+++ main.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Kamil Dłutowski on 12/11/2018.
+//
+
+#include <iostream>
+#include <math.h>
+#include <string>
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include "klasy.h"
+
+int main(int argc, char* argv[]) {
+    testing::InitGoogleTest(&argc, argv);
+
+    RUN_ALL_TESTS();
+    return 0;
+}
